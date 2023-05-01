ad65075b17680cb9cca6fcf7258130c71b6f4f38
Wilson Ayoola <ayoolawilson771@gmail.com>
2023-05-01 22:25:09 +0100

    Yes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer'; 
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Wagyu Beef",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Kosher Salt",
+    },
+    {
+      id: 4,
+      checked: false,
+      item: "Chocolate Bread"
+    },
+    {
+      id: 5,
+      checked: false,
+      item: "Sardines"
+    }
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? {
+      ...item, checked: !item.checked
+    }: item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
+
+  const handleDelete = (id) => {
+     const listItems = items.filter((item) => item.id !== id);
+     setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{"Wilson"}</p>
-      </header>
+      <Header  title='Groceries'/>
+      <Content 
+        items = {items}
+        handleCheck = {handleCheck}
+        handleDelete = {handleDelete}
+      />
+      <Footer 
+        length= {items.length}
+      />
     </div>
   );
 }
